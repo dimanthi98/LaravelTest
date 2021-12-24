fd897e2b42b17649e09e9c9bd670ab62b6b5217b
Ishani Dimanthi <i.dimanthi1998@gmail.com>
2021-12-24 15:09:09 -0800

    First commit

---
+++ resources/views/update.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Update</title>
+</head>
+<body>
+    <form action="/update" method="post">
+        @csrf
+        <input type="hidden" name="id" value="{{$data['id']}}">
+        <input type="text" name="fname" value="{{$data['fname']}}"><br><br>
+        <input type="text" name="lname" value="{{$data['lname']}}"><br><br>
+        <input type="text" name="email" value="{{$data['email']}}"><br><br>
+        <input type="submit" value="Update"><br><br>
+    </form>
+</body>
+</html>
